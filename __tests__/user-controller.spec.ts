be8b0371d06dee79d2a3e751fe88aa8271c3ece6
Bruno <brunognovaes.cm@gmail.com>
2022-02-02 10:07:40 -0300

    ensure that when searching by id return the correct user

--- __tests__/user-controller.spec.ts
+++ __tests__/user-controller.spec.ts
@@ -7,29 +7,42 @@ type User = {
 
 class UserController {
   constructor(private readonly userRepository: UserRepository) {}
+
   async create(user: User): Promise<User> {
     const userCreated = await this.userRepository.create(user)
     return userCreated
+  }
+
+  async findById(id: number): Promise<User | null> {
+    const userFinded = await this.userRepository.findById(id)
+    return userFinded
   }
 }
 
 interface UserRepository {
   create: (user: User) => Promise<User>
+  findById: (id: number) => Promise<User>
 }
 
 class UserRepositoryMock implements UserRepository {
-  user?: User
+  users?: User[]
   userId: number
   constructor() {
     this.userId = 1
+    this.users = []
   }
 
   async create (user: User): Promise<User> {
     user.id = this.userId
-    this.user = user
+    this.users.push(user)
     this.userId += 1
 
-    return this.user
+    return user
+  }
+
+  async findById (id: number): Promise<User | null> {
+    const userFinded = this.users.find((user) => user.id === id)
+    return userFinded ? userFinded : null
   }
 }
 
@@ -42,14 +55,25 @@ const makeSut = () => {
   }
 }
 
+const fakeUser1: User = { name: 'Bruno', password: '123456', email: 'bucgomes@gmail.com' }
+const fakeUser2: User = { name: 'Breno', password: '123456', email: 'becgomes@gmail.com' }
+
 describe('UserController create user', () => {
   it('should create with valid infos', async () => {
-    const {sut, repository} = makeSut()
-    const fakeUser: User = { name: 'Bruno', password: '123456', email: 'bucgomes@gmail.com' }
+    const { sut, repository } = makeSut()
+    
+    const userCreated = await sut.create(fakeUser1)
 
-    const userCreated = await sut.create(fakeUser)
-
-    expect(userCreated).toBe(repository.user)
+    expect(repository.users).toContain(userCreated)
     expect(userCreated.id).toBe(1)
+  })
+
+  it('should find a User by id', async () => {
+    const { sut } = makeSut()
+
+    const userCreated = await sut.create(fakeUser1)
+    const userFinded = await sut.findById(userCreated.id)
+    
+    expect(userFinded).toBe(userCreated)
   })
 })
